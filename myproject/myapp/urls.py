3bb082df6e0b2bfbca72296e851adbcf2a569e9f
Fortune Precious <fortuneprecious17@gmail.com>
2024-03-07 09:24:38 +0100

    restructured my code

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.home),
+  path('date_joined', views.display_date)
+]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
-from django.http import HttpResponse
-
 def home(request):
   return HttpResponse('Hello, World!')
+
+def display_date(request):
+  date_joined = datetime.today()
+  return HttpResponse(date_joined)
+
+def menu(request):
