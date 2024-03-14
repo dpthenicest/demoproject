7f090c55bd6af016d991eeec612cbbd412bcef77
Fortune Precious <fortuneprecious17@gmail.com>
2024-03-14 16:19:56 +0100

    added name to the urlpatterns

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home),
-  path('date_joined/', views.display_date),
-  path('menu/', views.menu),
-  path('path/', views.path),
-  path('req_info', views.request_info),
-  path('dishes/<str:dish>', views.menuitems)
+  path('', views.home, name='home'),
+  path('date_joined/', views.display_date, name='date_joined'),
+  path('menu/', views.menu, name='menu'),
+  path('path/', views.path, name='path'),
+  path('req_info/', views.request_info, name='request_info'),
+  path('dishes/<str:dish>/', views.menuitems, name='dishes_dish')
 ]
