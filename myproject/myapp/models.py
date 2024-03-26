1ec3d54c2e745457cbca09ed8be5f53ede6b5d49
Fortune Precious <fortuneprecious17@gmail.com>
2024-03-26 08:29:55 +0100

    menu and person models created

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,18 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+  name = models.CharField(max_length=100)
+  cuisine = models.CharField(max_length=100)
+  price = models.IntegerField()
+  size = models.CharField(max_length=1, default='M')
+
+  def __str__(self):
+    return self.name + ": " + self.cuisine
+  
+class Person(models.Model):
+  name = models.CharField(max_length=40)
+  email = models.EmailField(max_length=50)
+  phone_no = models.IntegerField()
