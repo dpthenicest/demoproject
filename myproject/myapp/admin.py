4303e5e33ae21650406968c0962cf3f1b05257d3
Fortune Precious <fortuneprecious17@gmail.com>
2024-03-26 10:01:34 +0100

    one-to-many rel

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Menu, Person
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Menu)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,18 +1,19 @@
-from unicodedata import name
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=50)
+    phone_no = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 class Menu(models.Model):
-  name = models.CharField(max_length=100)
-  cuisine = models.CharField(max_length=100)
-  price = models.IntegerField()
-  size = models.CharField(max_length=1, default='M')
+    name = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.CharField(max_length=1, default='M')
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, null=True, blank=True)
 
-  def __str__(self):
-    return self.name + ": " + self.cuisine
-  
-class Person(models.Model):
-  name = models.CharField(max_length=40)
-  email = models.EmailField(max_length=50)
-  phone_no = models.IntegerField()
+    def __str__(self):
+        return f"{self.name}: {self.cuisine}"
