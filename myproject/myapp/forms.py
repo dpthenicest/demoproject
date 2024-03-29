096954e00d875aa0d1b6882a1dc6e3c70cba594a
Fortune Precious <fortuneprecious17@gmail.com>
2024-03-29 18:11:19 +0100

    added form

---
+++ myproject/myapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms.widgets import NumberInput
+
+FAVORITE_DISH = [
+  ('italian', 'Italian'),
+  ('greek', 'Greek'),
+  ('turkish', 'Turkish'),
+]
+
+class DemoForm(forms.Form):
+  name = forms.CharField(widgets=forms.Textarea(attrs={'rows': 5}))
+  email = forms.EmailField(label="Enter email address")
+  reservation_date = forms.DateField(widget=NumberInput(attrs={'type': 'date'}))
+  favorite_dish = forms.ChoiceField(choices=FAVORITE_DISH)
