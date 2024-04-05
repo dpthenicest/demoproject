f41756bddfac89ca5319a11b8e31d91d14ebca07
Fortune Precious <fortuneprecious17@gmail.com>
2024-04-05 16:09:37 +0100

    fixed error in mysqlclient and db connection

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,13 +75,26 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {   
+    'default': {   
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',   
+        'USER': 'root',   
+        'PASSWORD': 'DP307&9As',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        }   
+    }   
+}   
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
